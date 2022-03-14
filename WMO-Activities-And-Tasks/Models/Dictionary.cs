23bfd0d546e71a1ef80dae7d1ec836dda7b5a38f
mwawrzyniiak <falers0@o2.pl>
2022-03-14 17:32:59 +0100

    add-csv

--- WMO-Activities-And-Tasks/Models/Dictionary.cs
+++ WMO-Activities-And-Tasks/Models/Dictionary.cs
@@ -2,6 +2,6 @@
 {
     public class Dictionary
     {
-        public static string CSV_PATH = @"D:\STUDIA-WAT\semestr10\WMO\23_WMO_Czynnosci_i_Zadania6_1.csv";
+        public static string CSV_PATH = "Resources\\23_WMO_Czynnosci_i_Zadania_Comp.csv";
     }
 }
