b0fcfc983f95c2e43d8bac5df6a027db1b0e90bd
mwawrzyniiak <falers0@o2.pl>
2022-03-14 16:15:34 +0100

    Add project files.

---
+++ WMO-Activities-And-Tasks/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WMO_Activities_And_Tasks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
